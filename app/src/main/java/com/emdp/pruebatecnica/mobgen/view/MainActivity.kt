bba04b55a79006fa0f9dea9e95f0cfa49213dfe0
Emma Dieguez <emmadieguezpaz@gmail.com>
2021-08-02 19:39:32 +0200

    New Splash Screen

--- app/src/main/java/com/emdp/pruebatecnica/mobgen/view/MainActivity.kt
+++ app/src/main/java/com/emdp/pruebatecnica/mobgen/view/MainActivity.kt
@@ -1,10 +1,15 @@
-package com.emdp.pruebatecnica.mobgen
+package com.emdp.pruebatecnica.mobgen.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emdp.pruebatecnica.mobgen.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_PruebaTecnicaMobgen)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
