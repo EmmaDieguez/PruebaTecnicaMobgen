6bfb1d5e2ec815ff50afc807fa21005fcf68a553
Emma Dieguez <emmadieguezpaz@gmail.com>
2021-08-04 21:12:51 +0200

    Insert api data into database
    Create a new database classes and insert data in it

--- app/src/main/java/com/emdp/pruebatecnica/mobgen/view/SplashActivity.kt
+++ app/src/main/java/com/emdp/pruebatecnica/mobgen/view/SplashActivity.kt
@@ -4,22 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
-import com.emdp.pruebatecnica.mobgen.model.api.CategoriesResponse
 import com.emdp.pruebatecnica.mobgen.viewModel.SplashViewModel
+import java.io.Serializable
 
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var splashViewModel: SplashViewModel
-    private var categories: List<CategoriesResponse> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         splashViewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
-        splashViewModel.getCategories().observe(this, { catRespList ->
-            categories = catRespList
-
-            startActivity(Intent(this, MainActivity::class.java))
+        splashViewModel.getCategories().observe(this, { categoriesList ->
+            val intent = Intent(this, MainActivity::class.java).apply {
+                putExtra("categories", categoriesList as Serializable)
+            }
+            startActivity(intent)
         })
     }
 }
--- app/src/main/java/com/emdp/pruebatecnica/mobgen/viewModel/SplashViewModel.kt
+++ app/src/main/java/com/emdp/pruebatecnica/mobgen/viewModel/SplashViewModel.kt
@@ -1,9 +1,11 @@
 package com.emdp.pruebatecnica.mobgen.viewModel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.emdp.pruebatecnica.mobgen.model.api.CategoriesResponse
+import com.emdp.pruebatecnica.mobgen.database.GameOfThronesDb
+import com.emdp.pruebatecnica.mobgen.model.database.Categories
 import com.emdp.pruebatecnica.mobgen.service.ApiService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -14,9 +16,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-class SplashViewModel : ViewModel() {
+class SplashViewModel(app: Application) : AndroidViewModel(app) {
 
-    private var categories: MutableLiveData<List<CategoriesResponse>> = MutableLiveData()
+    private var categories: MutableLiveData<List<Categories>> = MutableLiveData()
+    private var categoriesList: MutableList<Categories> = arrayListOf()
 
     private var httpLoggingInterceptor = HttpLoggingInterceptor()
         .setLevel(HttpLoggingInterceptor.Level.HEADERS)
@@ -39,17 +42,41 @@ class SplashViewModel : ViewModel() {
         loadCategories()
     }
 
-    fun getCategories() : LiveData<List<CategoriesResponse>> {
+    fun getCategories() : LiveData<List<Categories>> {
         return categories
     }
 
     private fun loadCategories() {
         CoroutineScope(Dispatchers.IO).launch {
             val call = retrofit.create(ApiService::class.java).getCategories()
-            val categoriesList = call.body()
+            val categoriesResponseList = call.body()
 
             run {
                 if (call.isSuccessful) {
+                    val categoriesDao = GameOfThronesDb
+                        .getAppDatabase(getApplication())?.categoriesDao()
+                    var category: Categories
+
+                    categoriesResponseList?.forEach {
+                        category = Categories(it.categoryName, it.type)
+                        categoriesList.add(category)
+                    }
+
+                    // Insert data into database
+                    val categoriesDb : List<Categories>? = categoriesDao?.getAllCategories()
+
+                    if (null == categoriesDb || categoriesDb.isEmpty()) {
+                        categoriesDao?.insertCategories(categoriesList)
+                    } else {
+                        // FIXME - Optimize this function
+                        categoriesList.forEach { category ->
+                            if (null == categoriesDao?.getCategoryByType(category.type)) {
+                                categoriesDao?.insertCategory(category)
+                            }
+                        }
+                    }
+
+                    // Send to view a list of categories
                     categories.postValue(categoriesList)
                 }
             }
